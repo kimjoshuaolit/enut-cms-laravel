3dead351421a6f1b39fd78d3bf3254f5bb343dbe
Kim Joshua Olit <kimjoshuaolit@gmail.com>
2026-06-10 13:38:44 +0800

    added force https on appserviceprovder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use App\View\Components\profile\ProfileCard;
 use App\View\Components\profile\PersonalInfoCard;
 use App\View\Components\enutrition\EnutritionMetrics;
 use App\View\Components\header\UserDropdown;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,9 +25,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         Blade::component('enutrition.enutrition-metrics', EnutritionMetrics::class);
- 	 Blade::component('header.user-dropdown', UserDropdown::class);
-Blade::component('profile.profile-card', ProfileCard::class);
-Blade::component('profile.personal-info-card', PersonalInfoCard::class);
+        Blade::component('enutrition.enutrition-metrics', EnutritionMetrics::class);
+        Blade::component('header.user-dropdown', UserDropdown::class);
+        Blade::component('profile.profile-card', ProfileCard::class);
+        Blade::component('profile.personal-info-card', PersonalInfoCard::class);
+
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
