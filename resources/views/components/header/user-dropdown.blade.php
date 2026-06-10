897bc36c47f2e61e54dea8b626c97c1df7265c3c
Kim Joshua Olit <kimjoshuaolit@gmail.com>
2026-06-10 15:15:45 +0800

    fix(dashboard): rename admin dropdown option to Profile. Changes the admin profile dropdown text from 'Edit Profile' to 'Profile' to match the expected design requirements.

--- resources/views/components/header/user-dropdown.blade.php
+++ resources/views/components/header/user-dropdown.blade.php
@@ -48,7 +48,7 @@
             @php
                 $menuItems = [
                     [
-                        'text' => 'Edit profile',
+                        'text' => 'Profile',
                         'icon' => '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 fill-rule="evenodd"
